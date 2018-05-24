a2ae72a946fb58af7695a92d3562478391ab80bd
Sridevi <sridevi@techjini.com>
2018-05-24 15:37:17 +0530

    string builder class

--- src/StringBuilderPage.java
+++ src/StringBuilderPage.java
@@ -1,3 +1,5 @@
+import org.omg.Messaging.SYNC_WITH_TRANSPORT;
+
 import java.util.Scanner;
 
 public class StringBuilderPage {
@@ -16,14 +18,28 @@ public class StringBuilderPage {
         else
             System.out.println(str1+" "+str2+" are different");
 
+        System.out.println("Please enter string to check palindrome");
+        String str3 = sc.next();
+        checkPalindrome(str3);
     }
 
     public static boolean stringCompare(String str1, String str2)
     {
-        System.out.println("In compare method");
+        System.out.println("In string compare method");
         if(str1.equals(str2))
             return true;
 
         return false;
+    }
+
+    public static void checkPalindrome(String str)
+    {
+        StringBuilder sb = new StringBuilder(str);
+        String revStr = sb.reverse().toString();
+
+        if(str.equals(revStr))
+            System.out.println("String is palindrome");
+        else
+            System.out.println("String is not palindrome");
     }
 }
